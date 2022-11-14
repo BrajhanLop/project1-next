d02177512e6a430cb54b80935f55ba5ffc378b88
BrajhanLop <bralopezsuasnabar@gmail.com>
2022-11-13 21:38:57 -0500

    probando nextjs

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+// ESTA PAGINA VIENE A SER EL INDEX.HTML
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head>
+        <title>mi primera web de next</title>
+        </head>
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+
+    // console.log('hola gato');
+    return (
+        <div>
+            <header>
+                <ul>
+                    <li>Home</li>
+                    <li>About me</li>
+                </ul>
+            </header>
+        </div>
+    );
+};
+
+export default Home;
